a1c5789d408c153d54d1d0c004ac572a0a4e1776
lindsaygelle <lindsay.gelle@gmail.com>
2023-03-30 16:35:07 +1100

    Updated layer.py

--- layer.py
+++ layer.py
@@ -4,3 +4,8 @@ from typing import(Any, Dict)
 def lambda_response() -> Dict[str, Any]:
     """lambda_response"""
     return {}
+
+
+def lambda_response_ok() -> Dict[str, Any]:
+    """lamdba_response_ok"""
+    return lambda_response()
