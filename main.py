a70c93d4b23908b2cf2b5c7f98b5632b8ec724b3
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-03-30 23:17:21 +1100

    Dev (#7)
    * Initial commit
    
    * Pylint
    
    * Updated .\workflow\pip.yaml
    
    * Updated layer.py
    
    * Testing
    
    * Updated ./workflows/pip.yml
    
    * Editing workflows
    
    * Added tf impl

---
+++ main.py
@@ -0,0 +1,24 @@
+"""layer"""
+from typing import(
+    Any,
+    Dict)
+from http.client import (
+    BAD_REQUEST,
+    CREATED,
+    OK,
+    SERVICE_UNAVAILABLE)
+
+
+def lambda_response(status_code: (BAD_REQUEST|CREATED|OK|SERVICE_UNAVAILABLE)) -> Dict[str, Any]:
+    """lambda_response"""
+    return {"status_code": status_code}
+
+
+def lambda_response_created() -> Dict[str, Any]:
+    """lambda_response_created"""
+    return lambda_response(CREATED)
+
+
+def lambda_response_ok() -> Dict[str, Any]:
+    """lamdba_response_ok"""
+    return lambda_response(OK)
