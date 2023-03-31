4053aeb2a9924a79b8ebeff84bdb6b54d0edeca1
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-03-31 15:37:13 +1100

    Dev (#10)
    * Initial commit
    
    * Pylint
    
    * Updated .\workflow\pip.yaml
    
    * Updated layer.py
    
    * Testing
    
    * Updated ./workflows/pip.yml
    
    * Editing workflows
    
    * Added tf impl
    
    * Updating workflows and scripts
    
    * Workflow testing
    
    * Workflow & precommit testing
    
    * Editing workflows
    
    * Workflow experimentation
    
    * Workflow experimentation
    
    * Workflow experimentation
    
    * Workflow test
    
    * Workflow test
    
    * Workflow path
    
    * Autoformatting
    
    * Workflows
    
    * Workflow experimentation
    
    * Editing ./github/workflows/*.yml
    
    * Autoformatting (refs/heads/dev)

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 """layer"""
-from typing import(
-    Any,
-    Dict)
-from http.client import (
-    BAD_REQUEST,
-    CREATED,
-    OK,
-    SERVICE_UNAVAILABLE)
+from typing import Any, Dict
+from http.client import BAD_REQUEST, CREATED, OK, SERVICE_UNAVAILABLE
 
 
-def lambda_response(status_code: (BAD_REQUEST|CREATED|OK|SERVICE_UNAVAILABLE)) -> Dict[str, Any]:
+def lambda_response(
+    status_code: (BAD_REQUEST | CREATED | OK | SERVICE_UNAVAILABLE),
+) -> Dict[str, Any]:
     """lambda_response"""
     return {"status_code": status_code}
 
