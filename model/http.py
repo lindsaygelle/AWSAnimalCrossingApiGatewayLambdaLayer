f1a0510ff50f758756e48f2b72bfdef7bd865bce
lindsaygelle <lindsay.gelle@gmail.com>
2023-04-03 06:12:23 +0000

    Autoformatting (refs/heads/dev)

--- model/http.py
+++ model/http.py
@@ -4,14 +4,12 @@ from datetime import date, datetime
 from http.client import BAD_REQUEST, CREATED, OK, UNAUTHORIZED, SERVICE_UNAVAILABLE
 from typing import AnyStr, Literal, Optional, TypedDict, TypeVar
 
-MethodTypeT = TypeVar("MethodTypeT",
-                      Literal["GET"],
-                      Literal["PATCH"],
-                      Literal["POST"])
+MethodTypeT = TypeVar("MethodTypeT", Literal["GET"], Literal["PATCH"], Literal["POST"])
 
 
 class LinkTypeT(TypedDict):
     """LinkTypeT"""
+
     http_method: MethodTypeT
     href: str
     rel: str
@@ -19,6 +17,7 @@ class LinkTypeT(TypedDict):
 
 class BaseTypeT(TypedDict):
     """LinkTypeT"""
+
     id: (int | str)
     link: LinkTypeT
     resource: AnyStr
@@ -26,6 +25,7 @@ class BaseTypeT(TypedDict):
 
 class BaseDetailTypeT(BaseTypeT):
     """DetailTypeT"""
+
     created_at: (date | datetime)
     timezone: Optional[str]
     updated_at: (date | datetime)
@@ -33,6 +33,7 @@ class BaseDetailTypeT(BaseTypeT):
 
 class BaseResponseTypeT(TypedDict):
     """ResponseTypeT"""
+
     request_epoch: int
     request_id: uuid.UUID
     response_epoch: int
